2ef7bc63d780285c4461c228eca7e2b5e307c82d
Lunnatica <paulalopezantelo@gmail.com>
2024-07-27 14:34:38 +0100

    feat: add ball logic

--- src/components/Cup/Cup.test.tsx
+++ src/components/Cup/Cup.test.tsx
@@ -6,4 +6,36 @@ describe("Cup", () => {
     render(<Cup />);
     expect(screen.getByText("Cup")).toBeInTheDocument();
   });
+
+  describe("when the cup has the ball", () => {
+    describe("when the cup is lifted", () => {
+      it("should render the ball", () => {
+        render(<Cup hasBall isLifted />);
+        expect(screen.getByTestId("ball")).toBeInTheDocument();
+      });
+    });
+
+    describe("when the cup is not lifted", () => {
+      it("should not render the ball", () => {
+        render(<Cup hasBall />);
+        expect(screen.queryByTestId("ball")).not.toBeInTheDocument();
+      });
+    });
+  });
+
+  describe("when the cup does not have the ball", () => {
+    describe("when the cup is lifted", () => {
+      it("should not render the ball", () => {
+        render(<Cup isLifted />);
+        expect(screen.queryByTestId("ball")).not.toBeInTheDocument();
+      });
+    });
+
+    describe("when the cup is not lifted", () => {
+      it("should not render the ball", () => {
+        render(<Cup />);
+        expect(screen.queryByTestId("ball")).not.toBeInTheDocument();
+      });
+    });
+  });
 });
--- src/components/Cup/Cup.tsx
+++ src/components/Cup/Cup.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-export const Cup: React.FC = () => {
+interface CupProps {
+  hasBall?: boolean;
+  isLifted?: boolean;
+}
+
+export const Cup: React.FC<CupProps> = ({
+  hasBall = false,
+  isLifted = false,
+}) => {
   return (
     <div>
       <h1>Cup</h1>
+      {hasBall && isLifted && <div data-testid="ball">Ball</div>}
     </div>
   );
 };
