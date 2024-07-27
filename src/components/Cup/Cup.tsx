6220d25b5edc48113f686bea511312fd5987dcb9
Lunnatica <paulalopezantelo@gmail.com>
2024-07-27 19:37:07 +0100

    feat: cup is a button

--- src/components/Cup/Cup.tsx
+++ src/components/Cup/Cup.tsx
@@ -5,7 +5,9 @@ import { CupInterface } from "../../types/types";
 export const Cup: React.FC<CupInterface> = ({ id, hasBall, isLifted }) => {
   return (
     <div>
-      <div data-testid={`cup-${id}`}>Cup {id}</div>
+      <button type="button" data-testid={`cup-${id}`}>
+        Cup {id}
+      </button>
       {hasBall && isLifted && <div data-testid="ball">Ball</div>}
     </div>
   );
