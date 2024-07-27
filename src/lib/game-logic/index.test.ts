b0896b36e662d2430ae347dafb3e7e7cb2996600
Lunnatica <paulalopezantelo@gmail.com>
2024-07-27 17:31:47 +0100

    feat: create logic to exchangeTwoCups

--- src/lib/game-logic/index.test.ts
+++ src/lib/game-logic/index.test.ts
@@ -1,4 +1,4 @@
-import { chooseRandomCup } from "./";
+import { chooseRandomCup, exchangeTwoCups } from "./";
 
 describe("game-logic", () => {
   describe("chooseRandomCup", () => {
@@ -6,6 +6,24 @@ describe("game-logic", () => {
       const result = chooseRandomCup(3);
       expect(result).toBeGreaterThanOrEqual(0);
       expect(result).toBeLessThanOrEqual(2);
+    });
+  });
+
+  describe("exchangeTwoCups", () => {
+    it("should return the same array if it has less than 2 elements", () => {
+      const cups = [{ id: 1, hasBall: false, isLifted: false }];
+      const result = exchangeTwoCups(cups);
+      expect(result).toEqual(cups);
+    });
+
+    it("should exchange two cups at random", () => {
+      const cups = [
+        { id: 1, hasBall: false, isLifted: false },
+        { id: 2, hasBall: true, isLifted: false },
+      ];
+      const result = exchangeTwoCups(cups);
+      expect(result).not.toEqual(cups);
+      expect(result[0]).toEqual(cups[1]);
     });
   });
 });
--- src/lib/game-logic/index.ts
+++ src/lib/game-logic/index.ts
@@ -1,3 +1,27 @@
+import { CupInterface } from "../../types/types";
+
 export const chooseRandomCup = (numberOfCups: number) => {
   return Math.floor(Math.random() * numberOfCups);
+};
+
+export const exchangeTwoCups = (initialCups: CupInterface[]) => {
+  const cups = [...initialCups];
+  if (cups.length < 2) {
+    return cups;
+  }
+
+  const randomIndex1 = Math.floor(Math.random() * cups.length);
+  let randomIndex2 = Math.floor(Math.random() * cups.length);
+
+  const maxAttempts = 10;
+  let attempts = 0;
+  while (randomIndex2 === randomIndex1 && attempts < maxAttempts) {
+    randomIndex2 = Math.floor(Math.random() * cups.length);
+  }
+
+  const temp = cups[randomIndex1];
+  cups[randomIndex1] = cups[randomIndex2];
+  cups[randomIndex2] = temp;
+
+  return cups;
 };
